08e8881e4aa610a0970a3f0bf147c902d6add82d
Jacob <jacobcons@gmail.com>
2024-08-31 08:21:46 +0000

    backend init

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL
+}))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import {ref} from 'vue';
+import {api} from '@/utils.js';
+
+const test = ref('loading')
+test.value = await api.get('/')
+
 </script>
 
 <template>
-   <h1 class="font-bold text-3xl">Home</h1>
+   <h1 class="font-bold text-3xl">{{ test }}</h1>
 </template>
