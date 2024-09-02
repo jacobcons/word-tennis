de7dc49619572dba0c4f080f9aef5b86daf88162
Jacob <jacobcons@gmail.com>
2024-09-02 13:23:18 +0000

    cors

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import {ref} from 'vue';
-import {api} from '@/utils.js';
+import { onMounted, ref } from 'vue'
+import { api } from '@/utils.js'
 
 const test = ref('loading')
-test.value = await api.get('/')
-
+onMounted(async () => {
+  test.value = (await api.get('/')).data
+})
 </script>
 
 <template>
-   <h1 class="font-bold text-3xl">{{ test }}</h1>
+  <h1 class="font-bold text-3xl">{{ test }}</h1>
+  <p>testtest</p>
 </template>
