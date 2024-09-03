b9b5586f94202c9febe512c58897668373b6d42e
Jacob <jacobcons@gmail.com>
2024-09-03 15:40:52 +0000

    search view + nicer styles for everything

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,37 +1,44 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { api } from '@/utils.js'
+import router from '@/router/index.js'
 
 const nickname = ref('')
+// create player and go to search view
 async function start() {
-  const { sessionId } = (
-    await api.post<{ sessionId: string }>('/players', {
+  const { sessionId, nickname: nicknameResponse } = (
+    await api.post<{ sessionId: string; nickname: string }>('/players', {
       nickname: nickname.value
     })
   ).data
   localStorage.setItem('sessionId', sessionId)
+  localStorage.setItem('nickname', nicknameResponse)
+  router.push({ path: '/search' })
 }
 </script>
 
 <template>
-  <div class="mx-auto mt-12 max-w-lg text-center">
-    <h1 class="mb-6">Word Tennis</h1>
-    <p class="mb-6">
-      This is a multiplayer browser game in which users must type related words back and fourth
-      under timed pressure. For example 'wave' could be followed by 'ocean', 'crash', 'sine' etc...
-    </p>
-    <input
-      v-model="nickname"
-      type="text"
-      placeholder="Enter Nickname..."
-      class="mb-6 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-    />
-    <button
-      @click="start"
-      type="submit"
-      class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    >
-      Start
-    </button>
+  <div class="flex h-screen w-screen items-center justify-center">
+    <div class="mx-auto flex max-w-lg flex-col items-center gap-y-6 text-center">
+      <h1 class="">Word Tennis</h1>
+      <p class="">
+        This is a multiplayer browser game in which users must type related words back and fourth
+        under timed pressure. For example 'wave' could be followed by 'ocean', 'crash', 'sine'
+        etc...
+      </p>
+      <input
+        v-model="nickname"
+        type="text"
+        placeholder="Enter Nickname..."
+        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+      />
+      <button
+        @click="start"
+        type="submit"
+        class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Start
+      </button>
+    </div>
   </div>
 </template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,46 @@ import flowbiteTypography from 'flowbite-typography'
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'h1, h2, h3, h4, h5, h6': {
+              marginTop: '0',
+              marginBottom: '0'
+            },
+            p: {
+              marginTop: '0',
+              marginBottom: '0'
+            },
+            ul: {
+              marginTop: '0',
+              marginBottom: '0'
+            },
+            ol: {
+              marginTop: '0',
+              marginBottom: '0'
+            },
+            blockquote: {
+              marginTop: '0',
+              marginBottom: '0'
+            },
+            img: {
+              marginTop: '0',
+              marginBottom: '0'
+            },
+            figure: {
+              marginTop: '0',
+              marginBottom: '0'
+            },
+            figcaption: {
+              marginTop: '0',
+              marginBottom: '0'
+            }
+          }
+        }
+      }
+    }
   },
   plugins: [flowbiteTypography]
 }
