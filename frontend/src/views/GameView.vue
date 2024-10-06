9d7070530243123dcae804984db1031cb14a190e
Jacob Cons <jacobcons@gmail.com>
2024-10-06 14:22:18 +0100

    disabled input styles

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -80,7 +80,7 @@ const currentWord = ref('')
           v-model="word"
           type="text"
           placeholder="Type Word..."
-          class="text-small block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          class="text-small block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 disabled:cursor-not-allowed disabled:bg-gray-200 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
           required
           :disabled="!currentPlayer.isYou"
         />
