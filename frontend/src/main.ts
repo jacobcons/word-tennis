2245d9e0cb824844c9409c73841cec29cb199366
Jacob Cons <jacobcons@gmail.com>
2024-11-02 16:24:37 +0000

    removed top level await connect to ws client

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,16 +7,15 @@ import App from './App.vue'
 import router from './router'
 import { addBearerTokenToAxios, connectToWsServer, socket } from '@/utils.js'
 
-const sessionId = localStorage.getItem('sessionId')
-
-if (sessionId) {
-  addBearerTokenToAxios(sessionId)
-  await connectToWsServer(sessionId)
-}
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+const sessionId = localStorage.getItem('sessionId')
+if (sessionId) {
+  addBearerTokenToAxios(sessionId)
+  connectToWsServer(sessionId)
+}
