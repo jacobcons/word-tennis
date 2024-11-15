eb66011d49f761620ac3c564b14ef621ac8d0b2e
Jacob Cons <jacobcons@gmail.com>
2024-11-15 13:47:28 +0000

    footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,5 +5,14 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <div class="format max-w-none">
     <RouterView />
+    <footer class="absolute bottom-4 left-1/2 -translate-x-1/2 transform text-center text-sm">
+      Made By
+      <a
+        href="https://jacobcons.com"
+        class="text-blue-700 no-underline hover:underline"
+        target="_blank"
+        >jacobcons.com</a
+      >
+    </footer>
   </div>
 </template>
