81e8cc725d03ab8772155bbdf8ec499e459f9b69
Jacob Cons <jacobcons@gmail.com>
2024-11-15 14:42:08 +0000

    added ga

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,24 @@
 import './style.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import { addBearerTokenToAxios, connectToWsServer, socket } from '@/utils.js'
+import VueGtag from 'vue-gtag'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(
+  VueGtag,
+  {
+    config: {
+      id: 'G-BWPVEB55J5'
+    }
+  },
+  router
+)
 
 app.mount('#app')
 
